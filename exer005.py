6b433b88cca2a1206fed47a68984516dc47f51ae
LeandroGessner <40524066+LeandroGessner@users.noreply.github.com>
2018-08-20 17:01:40 -0300

    Add files via upload

---
+++ exer005.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um número: '))
+numa = num - 1
+nums = num + 1
+print('O antecessor de {} é {} e o sucessor é {}'.format(num, numa, nums))
---
+++ exer006.py
@@ -0,0 +1,5 @@
+num = float(input('Digite um número: '))
+numd = num * 2
+numt = num * 3
+numrq = num ** (1/2)
+print('O dobro de {:.0f} é {:.0f}, o triplo é {:.0f} e a raiz quadrada é {:.3f}'.format(num, numd, numt, numrq))
